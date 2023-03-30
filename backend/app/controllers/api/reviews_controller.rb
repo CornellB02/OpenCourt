16093dd4bec246deac9700d4e748e8c46f49d43f
Cornell Bethea <betheacornellb@gmail.com>
2023-03-30 11:36:18 -0400

    live

--- backend/app/controllers/api/reviews_controller.rb
+++ backend/app/controllers/api/reviews_controller.rb
@@ -13,11 +13,29 @@ class Api::RestaurantsController < ApplicationController
         render :show
     end
 
-    
+    def create
+        @review = Review.new(review_params)
+
+        if @review.save
+            render :show
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @review = Review.find(params[:id])
+
+        if @review.update(review_params)
+            render :show
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
+    end
 
     private
 
     def review_params
-        params.require(:review).permit(:body, :overall, :food, :service, :ambience, :value)
+        params.require(:review).permit(:nickname, :body, :overall, :food, :service, :ambience, :value, :user_id, :restaurant_id)
     end
 end
