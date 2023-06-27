b87a737e74eab798016c4d74c6314619130c3e8a
Cornell Bethea <betheacornellb@gmail.com>
2023-06-27 03:43:05 -0400

    reservsbackendup

--- backend/app/models/reserv.rb
+++ backend/app/models/reserv.rb
@@ -1,5 +1,5 @@
 class Reserv < ApplicationRecord
-    validates :phone_number, presence: true, length: { is: 10 }, format: { with: /\A[0-9]{10}\z/, message: "Your phone number format is invalid." }
+    # validates :phone_number, presence: true, length: { is: 10 }, format: { with: /\A[0-9]{10}\z/, message: "Your phone number format is invalid." }
     validates_presence_of :restaurant_id, :user_id, :date, :time, :first_name
     validates :party_size, presence: true, numericality: { greater_than: 0 }, allow_blank: true
     # belongs_to :restaurant
