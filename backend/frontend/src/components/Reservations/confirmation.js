e0a32f86960b6fa95daa20ed212a5804474c5cb9
Cornell Bethea <betheacornellb@gmail.com>
2023-07-06 01:25:25 -0400

    confirmpage

--- backend/frontend/src/components/Reservations/confirmation.js
+++ backend/frontend/src/components/Reservations/confirmation.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import NavBar from "../Navigation/NavBar";
 
 function ReservationConfirmation() {
   const location = useLocation();
   const reservation = location.state?.reservation;
 
   return (
-    <div>
-      <h2>Reservation Confirmation</h2>
+    <div className="confirm-container">
+        {/* <NavBar> */}
+        <div className="nav-bar1">
+        <NavBar />
+        </div>
+        <div className="page-details">
+      <h5>Reservation Confirmation</h5>
       {reservation && (
         <div>
-          <p>Reservation Details:</p>
-          <p>Party Size: {reservation.party_size}</p>
-          <p>Date: {reservation.date}</p>
-          <p>Time: {reservation.time}</p>
+          <span className="detail">Reservation Details<p className="reserv_det"></p></span>
+          <span className="detail">Party Size<p className="reserv_det">{reservation.party_size}</p></span>
+          <span className="detail">Date<p className="reserv_det">{reservation.date}</p></span>
+          <span className="detail">Time<p className="reserv_det">{reservation.time}</p></span>
           {/* Add more details as needed */}
         </div>
       )}
+      </div>
     </div>
   );
 }
